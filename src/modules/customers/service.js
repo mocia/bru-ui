39ee926ee6a97c138c5a931f36f4b46dd0cdbd35
Lucia Widyana <mo_cia@yahoo.com>
2022-11-16 16:50:02 +0700

    bateeqshop-customer

--- src/modules/customers/service.js
+++ src/modules/customers/service.js
@@ -4,42 +4,78 @@ import { RestService } from "../../utils/rest-service";
 import { Container } from "aurelia-dependency-injection";
 import { Config } from "aurelia-api";
 
-const serviceUri = 'pageSize';
+const serviceUri = 'customer';
 export class Service extends RestService {
   constructor(http, aggregator, config, endpoint) {
-    super(http, aggregator, config, "customers");
+    super(http, aggregator, config, "sales");
   }
 
   getCustomers(args) {
-    let endpoint = "api/getUser";
+    let endpoint = `${serviceUri}`;
     return super.list(endpoint, args);
   }
 
-  search(args) {
-    let endpoint = "api/searchUser";
-    return super.list(endpoint, args);
+  search(info) {
+    var endpoint = `${serviceUri}`;
+    var query = '';
+    if (info.email && info.email !== "") {
+      if (query === '') query = `email=${info.email}`;
+      else query = `${query}&email=${info.email}`;
+  }
+  if (info.name && info.name !== "") {
+      if (query === '') query = `name=${info.name}`;
+      else query = `${query}&name=${info.name}`;
+  }
+  if (info.phoneNumber && info.phoneNumber !== "") {
+      if (query === '') query = `phoneNumber=${info.phoneNumber}`;
+      else query = `${query}&phoneNumber=${info.phoneNumber}`;
+  }
+  if (info.dobFrom && info.dobFrom !== "") {
+      if (query === '') query = `dobFrom=${info.dobFrom}`;
+      else query = `${query}&dobFrom=${info.dobFrom}`;
+  }
+  if (info.dobTo && info.dobTo !== "") {
+      if (query === '') query = `dobTo=${info.dobTo}`;
+      else query = `${query}&dobTo=${info.dobTo}`;
+  } 
+  if (info.membershipTier && info.membershipTier !== "") {
+    if (query === '') query = `membershipTier=${info.membershipTier}`;
+    else query = `${query}&membershipTier=${info.membershipTier}`;
+}
+  
+  if (query !== '')
+      endpoint = `${serviceUri}?${query}`;
+    return super.get(endpoint);
   }
 
   getCustomersById(id) {
-    var endpoint = `api/getUserDetail/${id}`;
+    var endpoint = `${serviceUri}/${id}`;
     var promise = this.endpoint.find(endpoint);
     this.publish(promise);
     return promise.then((result) => {
       this.publish(promise);
-      return Promise.resolve(result);
+      return Promise.resolve(result.data);
     });
   }
 
   getAddressBookById(id) {
-    var endpoint = `api/getAddressBook/${id}`;
+    var endpoint = `${serviceUri}/address/${id}`;
     var promise = this.endpoint.find(endpoint);
     this.publish(promise);
     return promise.then((result) => {
       this.publish(promise);
-      return Promise.resolve(result);
+      return Promise.resolve(result.data);
     });
   }
-
+  getOrderByUserId(id) {
+    var endpoint = `${serviceUri}/order/${id}`;
+    var promise = this.endpoint.find(endpoint);
+    this.publish(promise);
+    return promise.then((result) => {
+      this.publish(promise);
+      return Promise.resolve(result.data);
+    });
+  }
   generateExcel(dateFrom, dateTo) {
     var endpoint = `${serviceUri}/download?dateFrom=${dateFrom}&dateTo=${dateTo}`;
     return super.getXls(endpoint);
