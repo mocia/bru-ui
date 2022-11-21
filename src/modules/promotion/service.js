7c7d01d3cf18290298f2bcc0f4e60af0e17551ea
achensetyawati <achenzetya@gmail.com>
2022-11-21 16:43:38 +0700

    update Voucher general

--- src/modules/promotion/service.js
+++ src/modules/promotion/service.js
@@ -114,7 +114,54 @@ class ServiceProduct extends RestService {
   }
 }
 
+const voucherUri="vouchers";
+class VoucherService extends RestService {
+  constructor(http, aggregator, config, endpoint) {
+    super(http, aggregator, config, "sales");
+  }
+
+  getCustomers(args) {
+    let endpoint = `${voucherUri}`;
+    return super.list(endpoint, args);
+  }
+
+  search(info) {
+    var endpoint = `${voucherUri}`;
+    var query = '';
+    return super.list(endpoint,info);
+  //   if (info.email && info.email !== "") {
+  //     if (query === '') query = `email=${info.email}`;
+  //     else query = `${query}&email=${info.email}`;
+  // }
+  // if (info.name && info.name !== "") {
+  //     if (query === '') query = `name=${info.name}`;
+  //     else query = `${query}&name=${info.name}`;
+  // }
+  // if (info.phoneNumber && info.phoneNumber !== "") {
+  //     if (query === '') query = `phoneNumber=${info.phoneNumber}`;
+  //     else query = `${query}&phoneNumber=${info.phoneNumber}`;
+  // }
+  // if (info.dobFrom && info.dobFrom !== "") {
+  //     if (query === '') query = `dobFrom=${info.dobFrom}`;
+  //     else query = `${query}&dobFrom=${info.dobFrom}`;
+  // }
+  // if (info.dobTo && info.dobTo !== "") {
+  //     if (query === '') query = `dobTo=${info.dobTo}`;
+  //     else query = `${query}&dobTo=${info.dobTo}`;
+  // } 
+  // if (info.membershipTier && info.membershipTier !== "") {
+  //   if (query === '') query = `membershipTier=${info.membershipTier}`;
+  //   else query = `${query}&membershipTier=${info.membershipTier}`;
+}
+  
+//   if (query !== '')
+//       endpoint = `${serviceUri}?${query}`;
+//     return super.get(endpoint);
+//   }
+}
+
 export {
   Service,
   ServiceProduct,
+  VoucherService
 };
